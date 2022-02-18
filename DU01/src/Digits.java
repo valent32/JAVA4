791e7163d08f6169e9b5ac05f2de3af4259d8249
valent32 <mvalent98@gmail.com>
2022-02-18 18:16:52 +0100

    add DU01

---
+++ DU01/src/Digits.java
@@ -0,0 +1,14 @@
+
+public class Digits {
+
+    public static int calculate(int k, int b) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculate(1, 9));
+        System.out.println(calculate(2, 5));
+        System.out.println(calculate(3, 5));
+        System.out.println(calculate(4, 3));
+    }
+}
